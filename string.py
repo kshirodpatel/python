016a90ed5018736180b7db20dcf5be9aa72e32e3
Kshirod Patel <kshirod.patel@gmail.com>
2025-11-27 13:03:34 +0530

    String manupulation

---
+++ string.py
@@ -0,0 +1,35 @@
+# text = "ant eggs"
+# text_1 = "again eggs"
+# print(text + text_1 ) #doesn't add space by its own
+# print(text, text_1) #adds a space for each variable
+
+# s = "First line. \"Second\" line." # back slash when want to use double quotes
+# print(s)
+
+# s1 = "first line"
+# s2 = "second line"
+
+# print(s1 + "\n" + s2) #When want add line after each variable
+# print(id(s2))
+# s2 = 'Again a line'
+
+# str = 3 * ("py" + "thon ") + "\n" + 2 * "learn "
+# print(str)
+# 
+# Reverse a string
+# str = "Hello world from Python!";
+# rev = ''
+# for s in str:
+#     rev = s + rev
+# print(rev)
+
+# rev = rev[::-1]
+# print(rev)
+
+#Replace a part of string
+# s = "Php is easy to learn"
+# s = s.replace("Php", "Python")
+# print(s)
+
+# s = 'Python'
+# print(s[0::2])
