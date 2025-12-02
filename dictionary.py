9816eeebcef4b967c93e704215f968940564e4b6
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-02 13:22:54 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,40 @@
+# Dictionary: Key-Value pairs, Unordered, Mutable
+
+# my_dict = {'name': 'Kshirod', 'title': 'Patel'}
+# print(type(my_dict))
+# new_dict = my_dict
+# print(my_dict)
+
+# new_dict['name'] = 'Rajesh'
+# print(new_dict)
+
+# another_dict = dict(name="Marry", age=22, city="Bengaluru")
+# print(another_dict)
+# print(type(another_dict))
+
+# if 'name1' in another_dict:
+#     print('Yes')
+# else:
+#     print('No')
+
+# for key, value in my_dict.items():
+#     print(key)
+#     print(value)
+
+# mydict = {"name":"Kshirod", "title": "Patel", "location": "India"}
+
+# another_dict = mydict
+# another_dict["name"] = "John"
+# print(another_dict)
+# print(mydict)
+
+# another_dict = mydict.copy()
+# another_dict["name"] = "John"
+# print(another_dict)
+# print(mydict)
+
+# Merge two dict
+# dict1 = {"name":"Kshirod", "location": "India", "Gender": "Male"}
+# dict2 = {"name":"Suresh", "age": 26}
+# dict1.update(dict2)
+# print(dict1)
